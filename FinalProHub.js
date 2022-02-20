02ed0db01c1c5debeab8e5f105cd587a27fb73c5
Michael.A.G <macwassleepy@gmail.com>
2022-02-19 22:40:53 -0700

    Add files via upload

---
+++ FinalProHub.js
@@ -0,0 +1,10 @@
+function spaceHubPage() {
+    //Here  is where the button code goes
+    //This code take the user to the UATData project
+    location.replace("UATSpaceData.html");
+}
+
+function softwareEngObjective() {
+    //Button take the user to the Software Engineering Objective page.
+    location.replace("FinalProObj.html");
+}
